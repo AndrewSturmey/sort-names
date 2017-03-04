53291a4ed6fe5989ebb61b04afdc1c81a705bcce
Andrew Sturmey <a.sturmey@hotmail.com>
2017-03-04 12:06:33 +1000

    just trying to coordinate vs with github

---
+++ sort-names/sort-names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace sort_names
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {  //open the text file with a streamreader
+                using (StreamReader sr = new StreamReader("C://Users/Andrew/Source/Repos/names.txt"))
+                {
+                    // write the stream to string and write to the console
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("the file could not be read");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
