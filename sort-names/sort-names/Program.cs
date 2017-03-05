83319369bc3b52f154b23fcb6e07c11d0dabc47d
Andrew Sturmey <a.sturmey@hotmail.com>
2017-03-05 15:24:14 +1000

    tweaked how app is started and how it deals with the created file

--- sort-names/sort-names/Program.cs
+++ sort-names/sort-names/Program.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace sort_names
 {
     class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+            //create a list to put the string pairs into
+            var list = new List<string>();            
             try
-            {  //open the text file with a streamreader
-                using (StreamReader sr = new StreamReader("C://Users/Andrew/Source/Repos/names.txt"))
+            {  //loop through the text file and read each line in turn 
+                foreach (string line in File.ReadLines(args[0]))
                 {
-                    // write the stream to string and write to the console
-                    String line = sr.ReadToEnd();
-                    Console.WriteLine(line);
+                    //add it to the dictionary as a pair
+                    list.Add(line);
+                    
                 }
+                //and now we sort the list...
+                list.Sort();
+                //and here we output it to the console
+                foreach (var pair in list)
+                {
+                    Console.WriteLine(pair);
+                }
+                //and this is where we want the sorted list to be stored
+                string path = @"c://Users/Andrew/Source/Repos/sort-names/sorted" + args[0];
+
+                // first check to see if the output file exists
+                if (!File.Exists(path))
+                {
+                    //If not, then create it
+                    File.WriteAllText(path, "");
+                    
+                }
+                else
+                {
+                    //if it does exist append the sorted list
+                    foreach (var pair in list)
+                    {
+                        File.AppendAllText(path, pair + Environment.NewLine);
+                    }
+                }
+                Console.WriteLine("Finished: created" + path);
+
+
             }
             catch (Exception e)
             {
